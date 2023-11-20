a6502ce73af181a8bb32c151f78cb7403d4114fe
Max Turk <maxturk95@gmail.com>
2023-11-20 15:07:36 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Write your functions here, make sure to export them
+// Write your functions here, make sure to export them  
 const double1 = (first) => first * 2;
 
 const double2 = (first, second) => first * 2;
